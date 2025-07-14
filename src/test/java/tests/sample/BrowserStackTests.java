9aa7bcd6bf2172917b9a544205ad9135088a3f31
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-07-14 15:25:37 +0300

    first WikipediaSampleTest

---
+++ src/test/java/tests/sample/BrowserStackTests.java
@@ -0,0 +1,36 @@
+package tests.sample;
+
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BrowserStackTests extends BstackRunner {
+
+    @Test
+    void androidSampleTest() throws Exception{
+        WebElement skipButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+                ExpectedConditions.presenceOfElementLocated(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")));
+        skipButton.click();
+
+        WebElement searchElement = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+                ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Search Wikipedia")));
+
+        searchElement.click();
+        WebElement insertTextElement = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+                ExpectedConditions.elementToBeClickable(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")));
+        insertTextElement.sendKeys("BrowserStack");
+        Thread.sleep(5000);
+
+        List<WebElement> allProductsName = driver.findElements(AppiumBy.className("android.widget.TextView"));
+        assertTrue(allProductsName.size() > 0);
+
+    }
+
+}
---
+++ src/test/java/tests/sample/BstackRunner.java
@@ -0,0 +1,95 @@
+package tests.sample;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BstackRunner {
+    public AndroidDriver driver;
+
+    public static String userName, accessKey;
+    public static Map<String, Object> browserStackYamlMap;
+    public static final String USER_DIR = "user.dir";
+
+    public BstackRunner() {
+        File file = new File(getUserDir() + "/browserstack.yml");
+        this.browserStackYamlMap = convertYamlFileToMap(file, new HashMap<>());
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        userName = System.getenv("BROWSERSTACK_USERNAME") != null
+                ? System.getenv("BROWSERSTACK_USERNAME")
+                : (String) browserStackYamlMap.get("userName");
+
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY") != null
+                ? System.getenv("BROWSERSTACK_ACCESS_KEY")
+                : (String) browserStackYamlMap.get("accessKey");
+
+        MutableCapabilities capabilities = new MutableCapabilities();
+
+        // Получаем первое устройство из platforms
+        List<Map<String, Object>> platforms = (List<Map<String, Object>>) browserStackYamlMap.get("platforms");
+        Map<String, Object> device = platforms.get(0);
+
+        // bstack:options - общие опции BrowserStack
+        Map<String, Object> bstackOptions = new HashMap<>();
+
+        // Устанавливаем app отдельно в capabilities (корневой уровень)
+        capabilities.setCapability("app", browserStackYamlMap.get("app"));
+
+        bstackOptions.put("deviceName", device.get("deviceName"));
+        bstackOptions.put("platformVersion", device.get("platformVersion"));
+        bstackOptions.put("platformName", device.get("platformName"));
+
+        bstackOptions.put("buildName", browserStackYamlMap.get("buildName"));
+        bstackOptions.put("projectName", browserStackYamlMap.get("projectName"));
+        bstackOptions.put("debug", browserStackYamlMap.get("debug"));
+        bstackOptions.put("networkLogs", browserStackYamlMap.get("networkLogs"));
+        bstackOptions.put("local", browserStackYamlMap.getOrDefault("browserstackLocal", false));
+        bstackOptions.put("source", "junit5:appium-sample-sdk:v1.1");
+        bstackOptions.put("buildIdentifier", browserStackYamlMap.get("buildIdentifier"));
+
+        capabilities.setCapability("bstack:options", bstackOptions);
+
+        String url = String.format("https://%s:%s@hub.browserstack.com/wd/hub", userName , accessKey);
+
+        driver = new AndroidDriver(new URL(url), capabilities);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null){
+
+            driver.quit();
+        }
+    }
+
+    private String getUserDir() {
+        return System.getProperty(USER_DIR);
+    }
+
+    private Map<String, Object> convertYamlFileToMap(File yamlFile, Map<String, Object> map) {
+        try {
+            InputStream inputStream = Files.newInputStream(yamlFile.toPath());
+            Yaml yaml = new Yaml();
+            Map<String, Object> config = yaml.load(inputStream);
+            map.putAll(config);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Malformed browserstack.yml file - %s.", e));
+        }
+        return map;
+    }
+}
